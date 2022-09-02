b9a1b4a1dd3f7b46c57bd9a097ed2dbd3e40af06
SKustassoo <44232116+SKustassoo@users.noreply.github.com>
2022-09-02 11:25:02 +0300

    tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(input, times) {
+    let output ="";
 
+    if(times >= 0){
+        for(i=0; i < times; i++){
+            output +=input;
+        }
+    } else {
+        output = "ERROR";
+    }
+
+
+
+    return output;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(input) {
 
+    let originalArray =[];
+    let reversedArray = [];
+    let reversedString ="";
+
+    originalArray = Array.from(input);
+
+    for (i = originalArray.length; i > 0; i--){
+        reversedArray.push(originalArray[i-1]);
+    }
+
+    reversedString = reversedArray.join('');
+
+    return reversedString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
